39619cbe509734b17dd17bba00bf6db82d289a31
Aakash Lokhande <aakash.slokhande.mst17@itbhu.ac.in>
2022-08-16 16:45:13 +0530

    succesful commit 2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 
 var websites = map[string]int{}
@@ -13,47 +14,45 @@ func main() {
 	websites["https://www.facebook.com"] = 0
 
 	fmt.Println("starting server...")
-	http.HandleFunc("/", handler)
 	http.HandleFunc("/GET/websites", check_websites)
-	go add()
+	go check_status()
 	run_server()
-	fmt.Println("nedt...")
 
 }
-func add() {
-	websites["https://www.youtube.com"] = 0
+func check_status() {
+	for {
+		fmt.Println("checking now...")
+		check_all_websites()
+		time.Sleep(60 * time.Second)
+	}
 }
 func run_server() {
 	http.ListenAndServe(":8080", nil)
 }
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+
 func check_websites(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	name, present := query["name"]
 	if !present || len(name) == 0 {
-		check_all_websites(w, r)
+		check_all_websites()
+		display_all_websites(w, r)
 	} else {
 		check_individual_website_now(w, r, "https://"+name[0])
 	}
 }
-func check_all_websites(w http.ResponseWriter, r *http.Request) {
+func check_all_websites() {
 	var wg sync.WaitGroup
 	wg.Add(len(websites))
 
 	for url, _ := range websites {
-		go check_individual_website(w, r, url, &wg)
+		go check_individual_website(url, &wg)
 
 	}
 	wg.Wait()
-	for url, _ := range websites {
-		fmt.Fprint(w, url, " : ", websites[url], "\n")
-	}
 
 }
 
-func check_individual_website(w http.ResponseWriter, r *http.Request, url string, wg *sync.WaitGroup) {
+func check_individual_website(url string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	resp, _ := http.Get(url)
 	websites[url] = resp.StatusCode
@@ -64,4 +63,10 @@ func check_individual_website_now(w http.ResponseWriter, r *http.Request, url st
 	websites[url] = resp.StatusCode
 
 	fmt.Fprint(w, url, " : ", websites[url], "\n")
+}
+
+func display_all_websites(w http.ResponseWriter, r *http.Request) {
+	for url, _ := range websites {
+		fmt.Fprint(w, url, " : ", websites[url], "\n")
+	}
 }
